cec7299591e88ca0f24b667c8b534a90b2e39b35
Juraci Vieira <juraci.vieira@gmail.com>
2017-05-04 22:57:13 -0300

    filtering zombies by weapon

--- app/controllers/zombies_controller.rb
+++ app/controllers/zombies_controller.rb
@@ -1,5 +1,9 @@
 class ZombiesController < ApplicationController
   def index
-    render json: Zombie.all, status: :ok
+    zombies = Zombie.all
+    if params[:weapon]
+      zombies = zombies.where(weapon: params[:weapon])
+    end
+    render json: zombies, status: :ok
   end
 end
--- spec/integration/zombies_spec.rb
+++ spec/integration/zombies_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe 'Zombies', type: :request do
+  before(:example) do
+    Zombie.delete_all
+  end
+
   context 'GET /zombies' do
     it 'returns zombies in the JSON API format' do
       Zombie.create!(name: 'Ash', weapon: 'Axe')
@@ -8,6 +12,20 @@ describe 'Zombies', type: :request do
       get '/zombies'
 
       expect(response.status).to eq 200
+    end
+  end
+
+  context 'GET /zombies?weapon=Axe' do
+    it 'returns only the zombies that have axes' do
+      Zombie.create!(name: 'Ash', weapon: 'Axe')
+      Zombie.create!(name: 'Ana', weapon: 'Shotgun')
+
+      get '/zombies?weapon=Axe'
+
+      expect(response.status).to eq 200
+      zombies = JSON.parse(response.body, symbolize_names: true)
+      expect(zombies[:data].length).to eq 1
+      expect(zombies[:data].first[:attributes][:name]).to eq 'Ash'
     end
   end
 end
